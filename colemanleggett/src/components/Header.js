58d74111ec5f9668d747da2aa1326aa811be76cf
coleleg <coleleg@gmail.com>
2022-04-14 14:41:07 -0400

    home background added. header in place.

---
+++ colemanleggett/src/components/Header.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Header() {
+    return (
+        <Nav>
+            <Name>
+            COLEMAN LEGGETT
+            </Name>
+            <Menu>
+                <a>
+                    <span>SKILLS</span>
+                </a>
+                <a>
+                    <span>ABOUT</span>
+                </a>
+                <a>
+                    <span>PROJECTS</span>
+                </a>
+                <a>
+                    <span>CONTACT</span>
+                </a>
+            </Menu>
+        </Nav>
+    )
+};
+
+const Nav = styled.div`
+    height: 70px;
+    top: 0;
+    left: 0;
+    right: 0;
+    background: #0A0A0A;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 24px;
+    overflow-x: hidden;
+    color: rgb(240, 247, 244);
+    position: fixed;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex: 1;
+    margin-left: 24px;
+    align-items: center;
+    justify-content: right;
+    
+    a {
+        display: flex;
+        align-items: center;
+        padding: 0 12px;
+        cursor: pointer;
+    }
+`;
+
+const Name = styled(Menu)`
+    justify-content: left;`;
+
+export default Header
--- colemanleggett/src/components/Home.js
+++ colemanleggett/src/components/Home.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import styled from 'styled-components';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Skills from './Skills';
 
 function Home() {
     return (
-        <div>Home</div>
+        <Container>
+            hello
+        </Container>
     )
-}
+};
+
+const Container = styled.main`
+    min-height: calc(100vh-70px);
+    padding: 0 calc(3.5vw + 5px);
+    overflow-x: hidden;
+    
+    &:before {
+        background: url("images/black-textured.jpg") center center / cover no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: -1;
+    }
+`;
+
 
 export default Home
